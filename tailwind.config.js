3ef74b83dfa08593db6dc8d4c9de5163464dbf16
heattechgy-glitch <heattech.gy@gmail.com>
2026-06-25 23:22:37 -0400

    [BOSS] Tailwind v3. module.exports = { content: ['./index.html','./src/**/*.{js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        'electric-blue': '#0ea5e9',
+      },
+    },
+  },
+  plugins: [],
+};
